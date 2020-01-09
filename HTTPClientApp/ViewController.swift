090175cbc4b91729dc72a443b55acfefe69f8923
Dumitru Tabara <dumitru.tabara@codwer.com>
2020-01-09 11:01:31 +0200

    Initial Commit

---
+++ HTTPClientApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HTTPClientApp
+//
+//  Created by Dumitru Tabara on 1/9/20.
+//  Copyright © 2020 Dumitru Tabara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
